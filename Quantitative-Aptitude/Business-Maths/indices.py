4d0b12a25995c238febd460f08032cf360e595a0
Haresh <99605763+ForceTheGod@users.noreply.github.com>
2026-03-14 03:36:16 +0530

    Create indices.py

---
+++ Quantitative-Aptitude/Business-Maths/indices.py
@@ -0,0 +1,11 @@
+#calculating powers using inbuilt pow function
+
+result = pow(6,-1)
+print(result)
+result2 = pow(2,6)
+print(result2)
+result3 = pow(54,3)
+print(result3)
+result4 = pow(15,0.7)
+print(result4)
+
